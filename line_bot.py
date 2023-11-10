e47b73ed3d2029bb8dd7c0615d4965cb69557549
piyo31 <job.sto.h@gmail.com>
2023-11-10 23:05:23 +0900

    update button template

--- line_bot.py
+++ line_bot.py
@@ -2,51 +2,51 @@ import os
 import requests
 
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-LINE_GROUP_ID = os.environ["LINE_GROUP_ID"]  # 追加
+LINE_GROUP_ID = os.environ["LINE_GROUP_ID"]
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}"
+}
+
+data = {
+    "to": LINE_GROUP_ID,
+    "messages": [
+        {
+            "type": "template",
+            "altText": "明日の定例会は参加する？",
+            "template": {
+                "type": "buttons",
+                "thumbnailImageUrl": "ここに画像のURLを挿入",  # 画像がある場合
+                "imageAspectRatio": "rectangle",
+                "imageSize": "cover",
+                "imageBackgroundColor": "#FFFFFF",
+                "title": "定例会の出欠確認",
+                "text": "明日の定例会、参加する？",
+                "actions": [
+                    {
+                        "type": "message",
+                        "label": "参加",
+                        "text": "参加するよ！"
+                    },
+                    {
+                        "type": "message",
+                        "label": "欠席",
+                        "text": "ごめん､欠席する..."
+                    },
+                    {
+                        "type": "message",
+                        "label": "未定(いつ分かるか追記してね)",
+                        "text": "未定だなぁ...いつ分かるか5秒後に送るから待ってて！"
+                    }
+                ]
+            }
+        }
+    ]
+}
 
 def send_line_message():
     url = "https://api.line.me/v2/bot/message/push"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}"
-    }
-    data = {
-        "to": LINE_GROUP_ID,  # 変更
-        "messages": [
-            {
-                "type": "text",
-                "text": "明日の定例会は参加する？",
-                "quickReply": {
-                    "items": [
-                        {
-                            "type": "action",
-                            "action": {
-                                "type": "message",
-                                "label": "参加",
-                                "text": "参加"
-                            }
-                        },
-                        {
-                            "type": "action",
-                            "action": {
-                                "type": "message",
-                                "label": "欠席",
-                                "text": "欠席"
-                            }
-                        },
-                        {
-                            "type": "action",
-                            "action": {
-                                "type": "message",
-                                "label": "未定",
-                                "text": "未定（いつわかるかも追記してね）"
-                            }
-                        }
-                    ]
-                }
-            }
-        ]
-    }
     response = requests.post(url, headers=headers, json=data)
     print(response.status_code, response.json())
 
