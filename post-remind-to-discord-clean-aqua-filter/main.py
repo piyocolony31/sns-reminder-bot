1edf3f34e6d316fb378e79a0cd5a211347bb669b
piyocolony31 <40974762+piyocolony31@users.noreply.github.com>
2024-07-08 08:25:54 +0900

    aa

--- post-remind-to-discord-clean-aqua-filter/main.py
+++ post-remind-to-discord-clean-aqua-filter/main.py
@@ -11,20 +11,20 @@ def main(token, content, button_label, button_response):
     @bot.event
     async def on_ready():
         print(f'Logged in as {bot.user}')
+        await send_button()
+        await bot.close()
 
-    @bot.command(name="send_button")
-    async def send_button(ctx):
+    async def send_button():
+        channel = bot.get_channel(bot.guilds[0].text_channels[0].id)
         button = discord.ui.Button(label=button_label, style=discord.ButtonStyle.primary, custom_id="button_click")
-
+        
         async def button_callback(interaction):
             await interaction.response.send_message(button_response, ephemeral=True)
-
+        
         button.callback = button_callback
-
         view = discord.ui.View()
         view.add_item(button)
-
-        await ctx.send(content, view=view)
+        await channel.send(content, view=view)
 
     bot.run(token)
 
@@ -32,10 +32,10 @@ if __name__ == "__main__":
     if len(sys.argv) != 5:
         print("Usage: python post-remind-to-discord-clean-aqua-filter/main.py <token> <content> <button_label> <button_response>")
         sys.exit(1)
-
+    
     token = sys.argv[1]
     content = sys.argv[2]
     button_label = sys.argv[3]
     button_response = sys.argv[4]
-
+    
     main(token, content, button_label, button_response)
